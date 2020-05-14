b05ffdabc9c39fc4e7f1010590b521346b2ef717
DATA8320-11 <DATA8320-11@DESKTOP-5F38AMB>
2020-05-14 13:39:24 +0900

    산술 연술자

---
+++ JavaPrj/src/Hello9.java
@@ -0,0 +1,21 @@
+
+public class Hello9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a, b = 5,c = 3;
+		
+		a = b+c;
+		System.out.printf("%d + %d = %d\n", b, c, a);
+		a = b-c;
+		System.out.printf("%d - %d = %d\n", b, c, a);
+		a = b*c;
+		System.out.printf("%d * %d = %d\n", b, c, a);
+		a = b/c;
+		System.out.printf("%d / %d = %d\n", b, c, a);
+		a = b%c;
+		System.out.printf("%d %% %d = %d\n", b, c, a);
+
+	}
+
+}
