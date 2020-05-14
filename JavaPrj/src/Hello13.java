3baed09a39cd9ed6b5f278b4e8929fc05efd3d8c
DATA8320-11 <DATA8320-11@DESKTOP-5F38AMB>
2020-05-14 13:41:41 +0900

    논리 연산자

---
+++ JavaPrj/src/Hello13.java
@@ -0,0 +1,14 @@
+
+public class Hello13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =99;
+		
+		System.out.printf(" AND 연산 : %s \n", (a >= 100) && (a <=200));
+		System.out.printf(" OR 연산 : %s \n", (a >= 100) || (a <=200));
+		System.out.printf(" NOT 연산 : %s \n", !(a == 100));
+
+	}
+
+}
