a2daf1b8f50e7c1d0b0744be8b83a2c162f5abcd
DATA8320-11 <DATA8320-11@DESKTOP-5F38AMB>
2020-05-14 13:41:13 +0900

    증감연산자 ++

---
+++ JavaPrj/src/Hello12.java
@@ -0,0 +1,16 @@
+
+public class Hello12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10, b;
+		
+		b = a++;
+		System.out.printf("%d \n",b);
+		
+		b = ++a;
+		System.out.printf("%d \n", b);
+
+	}
+
+}
