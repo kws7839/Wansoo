e1465a3ae482762911c0dacbed3053df525d6dcd
DATA8320-11 <DATA8320-11@DESKTOP-5F38AMB>
2020-05-14 13:40:25 +0900

    증감연산자 대입연산자

---
+++ JavaPrj/src/Hello11.java
@@ -0,0 +1,31 @@
+
+public class Hello11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		
+		a++;
+		System.out.printf("a ++ ==> %d \n", a);
+		
+		a--;
+		System.out.printf("a -- ==> %d \n", a);
+		
+		a += 5;
+		System.out.printf("a += 5 ==> %d \n", a);
+		
+		a -= 5;
+		System.out.printf("a -= 5 ==> %d \n", a);
+		
+		a *= 5;
+		System.out.printf("a *= 5 ==> %d \n", a);
+		
+		a /= 5;
+		System.out.printf("a /= 5 ==> %d \n", a);
+		
+		a %= 5;
+		System.out.printf("a %%= 5 ==> %d \n", a);
+
+	}
+
+}
