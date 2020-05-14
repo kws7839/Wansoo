8555b9be497c16861e2fd71281bf72ddc3eabf73
DATA8320-11 <DATA8320-11@DESKTOP-5F38AMB>
2020-05-14 13:34:53 +0900

    과제

---
+++ JavaPrj/src/Hello5.java
@@ -0,0 +1,19 @@
+
+public class Hello5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("%d\n", 123);
+		System.out.printf("%5d\n", 123);
+		System.out.printf("%05d\n", 123);
+		
+		System.out.printf("%f\n", 123.45);
+		System.out.printf("%7.1f\n", 123.45);
+		System.out.printf("%7.3f\n", 123.45);
+		
+		System.out.printf("%s\n", "Hi~Java");
+		System.out.printf("%10s\n", "Hi~Java");
+	
+	}
+
+}
