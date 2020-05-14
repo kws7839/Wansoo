b505aeca45c8659feb3d4a9eefd35af67c29dc6b
DATA8320-11 <DATA8320-11@DESKTOP-5F38AMB>
2020-05-14 13:38:26 +0900

    변수에 변수대입

---
+++ JavaPrj/src/Hello8.java
@@ -0,0 +1,20 @@
+
+public class Hello8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a, b;
+		float c, d;
+		
+		a = 100;
+		b = a;
+		
+		c = 111.1f;
+		d = c;
+		
+		System.out.printf("a, b의 값 ==> %d, %d \n", a, b);
+		System.out.printf("c, e의 값 ==> %5.1f, %5.1f \n", c, d);
+
+	}
+
+}
