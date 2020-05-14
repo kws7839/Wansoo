662117efb02c3e667701c044853cc696867c727c
DATA8320-11 <DATA8320-11@DESKTOP-5F38AMB>
2020-05-14 13:44:45 +0900

    구구단

---
+++ JavaPrj/src/Hello19.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Hello19 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int i;
+		int dan;
+		
+		System.out.println("¸î´Ü");
+		dan = s.nextInt();
+		
+		for(i=1; i<=9; i++) {
+			System.out.printf("%d X %d = %d\n", dan, i, dan*i);
+		}
+
+	}
+
+}
