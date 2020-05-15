889117d47179fd19ed5302d093d5f3186d2e39ae
DATA8320-11 <DATA8320-11@DESKTOP-5F38AMB>
2020-05-15 11:06:34 +0900

    실습7-1

---
+++ JavaPrj/src/Ex07_01.java
@@ -0,0 +1,15 @@
+
+public class Ex07_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		i = 0;
+
+		while (i < 5) {
+			System.out.println("while 문을 공부홥니다 \n");
+			i++;
+		}
+	}
+
+}
