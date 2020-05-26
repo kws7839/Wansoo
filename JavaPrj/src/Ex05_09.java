881ff9b05c4ec453577a7ea6317800454201e654
DATA8320-11 <DATA8320-11@DESKTOP-5F38AMB>
2020-05-26 18:37:50 +0900

    switch case기초

---
+++ JavaPrj/src/Ex05_09.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Ex05_09 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a;
+		
+		System.out.println("1~4중에 선택하세요");
+		a = s.nextInt();
+
+		switch (a) {
+		case 1:
+			System.out.println("1을 선택했다\n");
+			break;
+		case 2:
+			System.out.println("2를 선택했다\n");
+			break;
+		case 3:
+			System.out.println("3을 선택했다\n");
+			break;
+		case 4:
+			System.out.println("4를 선택했다\n");
+			break;
+		default:
+			System.out.println("이상한걸 선택했다. \n");
+
+		}
+
+	}
+
+}
