a00e779b2b9f6c6c603cd24ef6595d36157ea60e
DATA8320-11 <DATA8320-11@DESKTOP-5F38AMB>
2020-05-26 17:59:57 +0900

    공부

---
+++ JavaPrj/src/예제10.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class 예제10 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int year;
+		
+		System.out.println("년도를 입력하세요 : ");
+		year = s.nextInt();
+		
+		if (((year % 4 ==0) && (year % 100 !=0)) || (year % 400 == 0))
+			
+			System.out.printf("%d 년은 윤년 입니다\n", year);
+		else
+			System.out.printf("%d 년은 윤년이 아닙니다\n", year);
+		
+
+	}
+
+}
