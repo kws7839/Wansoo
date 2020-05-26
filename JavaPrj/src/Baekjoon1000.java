133146ff9fc43d4b74c10d93e41218022b3ce73c
DATA8320-11 <DATA8320-11@DESKTOP-5F38AMB>
2020-05-26 18:31:27 +0900

    강아지그림

---
+++ JavaPrj/src/Baekjoon1000.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Baekjoon1000 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
+		System.out.printf("%d + %d = %d", a, b, a+b);
+		
+	}
+
+}
