8b85e6c09c61771d27a8c837058b2109504651da
DATA8320-11 <kws7839@naver.com>
2020-06-02 19:04:26 +0900

    공부

--- JavaPrj/src/Baekjoon10950.java
+++ JavaPrj/src/Baekjoon10950.java
@@ -4,14 +4,14 @@ public class Baekjoon10950 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		int Q = s.nextInt();
-		
+		int line = s.nextInt();
 
-		for (int i = 0; i < Q; i++) {
+		for (int i = 1; i <= line; i++) {
 			int a = s.nextInt();
 			int b = s.nextInt();
+
+			System.out.println(a + b);
 		}
-		System.out.println();
 	}
 
 }
