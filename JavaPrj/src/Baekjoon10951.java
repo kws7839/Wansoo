c1306762395e74741d8c999e7773b105e0f31270
DATA8320-11 <kws7839@naver.com>
2020-06-09 11:25:07 +0900

    와일문

---
+++ JavaPrj/src/Baekjoon10951.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Baekjoon10951 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		while (s.hasNextInt()) {
+			int a = s.nextInt();
+			int b = s.nextInt();
+
+			System.out.println(a + b);
+
+		}
+	}
+
+}
